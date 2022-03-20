819eec40f70de885dfbb3cb32bc68a9925eda241
AKASH C <76053029+akashsmart006@users.noreply.github.com>
2022-03-20 11:52:56 +0530

    Add files via upload

---
+++ DEMO1/website/views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+views = Blueprint('views',__name__)
+
+@views.route('/')
+def base():
+    return render_template("base.html")
+@views.route('/home1',methods=['GET','POST'])
+def home1():
+    return render_template("home1.html")
+@views.route('/home2',methods=['GET','POST'])
+def home2():
+    if request.method=='POST':
+        lat=request.form.get('lat')
+        lon=request.form.get('lon')
+        lat=float(lat)
+        lon=float(lon)
+        print(lat)
+        print(lon)
+    return render_template("home2.html")
+@views.route('/home3')
+def home3():
+    return render_template("home3.html")
+@views.route('/home4')
+def home4():
+    return render_template("home4.html")
